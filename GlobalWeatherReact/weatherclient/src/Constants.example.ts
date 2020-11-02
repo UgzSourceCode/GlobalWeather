2c21ed1be8c40aabecc6a51b83244e6edfc6c550
Piotr 'Ugz' Bielski <piotr.bielski@polishdev.com>
2020-11-02 02:30:00 +0100

    add Constants.ts file to gitignore

--- GlobalWeatherReact/weatherclient/src/Constants.example.ts
+++ GlobalWeatherReact/weatherclient/src/Constants.example.ts
@@ -3,5 +3,5 @@
     static citySearchAPIUrl = 'https://dataservice.accuweather.com/locations/v1/cities/';
     static currentConditionsAPIUrl = 'https://dataservice.accuweather.com/currentconditions/v1';
     static weatherIconUrl = 'http://developer.accuweather.com/sites/default/files/';
-    static apiKey = 'XXXXXXXXXXXXXXXXXX';
+    static apiKey = 'XXXXXXXXXXXXXXXXXX'; // your api key
 }
